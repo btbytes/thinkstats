4c1610819425e64ad3a11571cfacb5e1c460ca24
Pradeep Gowda <pradeep@btbytes.com>
2011-03-18 10:55:42 -0400

    first commit

---
+++ data/fetch_data.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+## Source http://greenteapress.com/thinkstats/nsfg.html
+
+#wget -c http://greenteapress.com/thinkstats/2002FemPreg.dat.gz
+#wget -c http://greenteapress.com/thinkstats/2002FemResp.dat.gz
+#wget -c http://greenteapress.com/thinkstats/2002Male.dat.gz
