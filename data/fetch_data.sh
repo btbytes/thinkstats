23a7315323dfc217da2be8edb10489e66407b142
Pradeep Gowda <pradeep@btbytes.com>
2011-03-19 23:19:44 -0400

    live birth stats

--- data/fetch_data.sh
+++ data/fetch_data.sh
@@ -2,6 +2,6 @@
 
 ## Source http://greenteapress.com/thinkstats/nsfg.html
 
-#wget -c http://greenteapress.com/thinkstats/2002FemPreg.dat.gz
-#wget -c http://greenteapress.com/thinkstats/2002FemResp.dat.gz
-#wget -c http://greenteapress.com/thinkstats/2002Male.dat.gz
+wget -c http://greenteapress.com/thinkstats/2002FemPreg.dat.gz
+wget -c http://greenteapress.com/thinkstats/2002FemResp.dat.gz
+wget -c http://greenteapress.com/thinkstats/2002Male.dat.gz
--- python/first.py
+++ python/first.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
-
+from __future__ import print_function
 import sys
 import survey
-0
-def main(name, data_dir="../data"):
+
+def num_pregs(data_dir="../data"):
     table = survey.Pregnancies()
     table.ReadRecords(data_dir)
-    print 'Number of Pregnancies: ', len(table.records)
+    print('Number of Pregnancies: ', len(table.records))
 
+def live_births(data_dir="../data"):
+    table = survey.Pregnancies()
+    table.ReadRecords(data_dir)
+    lb = [r for r in table.records if r.outcome == 1]
+    print('Number of live births: ', len(lb))
+
+def main(name, data_dir="../data"):
+    num_pregs()
+    live_births()
+    
 if __name__ == '__main__':
     main(*sys.argv)
-    
